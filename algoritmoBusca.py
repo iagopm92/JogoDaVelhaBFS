8787d1ed8cad63f9c716305dd1a1acab7eb419f4
Iago Mesquita <60303015+iagopm92@users.noreply.github.com>
2024-04-03 23:05:50 -0300

    Update algoritmoBusca.py

--- algoritmoBusca.py
+++ algoritmoBusca.py
@@ -6,14 +6,14 @@ def movimentoIA(board, jogador): # função responsável pela verificação do m
     melhor_valor = None # melhor valor iniciada com valor nulo
     melhor_movimento = None # melhor movimento iniciada com valor nulo
     for possibilidade in possibilidades: # verificação de possibilidades de jogadas pelo algoritmo
-        board[possibilidade[0]][possibilidade[1]] = token[jogador]
-        valor = algoritmoBusca(board, jogador)
+        board[possibilidade[0]][possibilidade[1]] = token[jogador] 
+        valor = algoritmoBusca(board, jogador) 
         board[possibilidade[0]][possibilidade[1]] = vazio
-        if(melhor_valor is None):
+        if(melhor_valor is None): # atribuição do melhor valor de jogada a variavel valor
             melhor_valor = valor
             melhor_movimento = possibilidade
-        elif(jogador == 0):
-            if(valor > melhor_valor):
+        elif(jogador == 0): # senao, é a vez do jogador 0
+            if(valor > melhor_valor): 
                 melhor_valor = valor
                 melhor_movimento = possibilidade
         elif(jogador == 1):
@@ -21,10 +21,10 @@ def movimentoIA(board, jogador): # função responsável pela verificação do m
                 melhor_valor = valor
                 melhor_movimento = possibilidade
 
-    return melhor_movimento[0], melhor_movimento[1]
+    return melhor_movimento[0], melhor_movimento[1] # retorno do melhor movimento para o algoritmo
 
 
-def getPosicoes(board):  
+def getPosicoes(board):  # verifica as posiçõs do tabuleiro e adiciona elas em na fila posicoes
     posicoes = []
     for i in range(3):
         for j in range(3):
@@ -34,15 +34,15 @@ def getPosicoes(board):
     return posicoes
 
 
-score = {
+score = {    # atribuição de um score a cada tipo de jogada
     "EMPATE": 0,
     "X": 1,
     "O": -1
 }
 
 # 
-def algoritmoBusca(board, jogador):
-    ganhador = verificaGanhador(board)
+def algoritmoBusca(board, jogador): # função de busca de melhor valor para realização da jogada pelo algoritmo
+    ganhador = verificaGanhador(board) 
     if(ganhador):
         return score[ganhador]
     jogador = (jogador + 1)%2
